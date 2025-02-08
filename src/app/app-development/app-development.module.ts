77ffce2c25431b62b476bd88a965362501ec4477
theayushkumar <ayushkumarhjp@gmail.com>
2025-02-08 18:52:53 +0530

    created modules

---
+++ src/app/app-development/app-development.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppDevelopmentRoutingModule } from './app-development-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppDevelopmentRoutingModule
+  ]
+})
+export class AppDevelopmentModule { }
---
+++ src/app/web-designing/web-designing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WebDesigningRoutingModule } from './web-designing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WebDesigningRoutingModule
+  ]
+})
+export class WebDesigningModule { }
