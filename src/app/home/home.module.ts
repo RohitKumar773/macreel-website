3df56d2be090947f417f2c48a27343f85da69899
theayushkumar <ayushkumarhjp@gmail.com>
2025-02-08 18:55:11 +0530

    created components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
