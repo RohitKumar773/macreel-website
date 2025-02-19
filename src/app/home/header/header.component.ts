7eda778770378187eeebaeefb7da873e043a042a
theayushkumar <ayushkumarhjp@gmail.com>
2025-02-19 19:10:02 +0530

    dd

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -7,15 +7,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private route: Router) {}
+  constructor(private router: Router) {}
 
   navigateTo(path: string) {
-    this.route.navigate([path]);
+    this.router.navigate([path]);
     scroll(0, 0);
   }
 
   scrollOnTop() {
     scroll(0, 0);
   }
+
+  isActive(paths: string | string[]): boolean {
+    if (typeof paths === 'string') {
+      return this.router.url === paths; // Exact match for single path
+    } else if (Array.isArray(paths)) {
+      return paths.some(path => this.router.url.startsWith(path)); // Match any of the paths
+    }
+    return false;
+  }
+  
+
 
 }
